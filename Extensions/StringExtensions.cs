95b552d273b0366b388d051ac0493164286e0ad9
GoodPro712 <charliebrannan154@gmail.com>
2020-06-05 11:55:32 +1000

    A

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AssortedModdingTools.Extensions
+﻿namespace AssortedModdingTools.Extensions
 {
 	public static partial class Extensions
 	{
-		public static void RemoveSpaces(this string str) => str.Replace(" ", "");
+		public static void RemoveSpaces(this string str)
+		{
+			str.Replace(" ", "");
+		}
 	}
 }
