d056a5ddccad26316bd6918b0705dd5124407f8e
LoLXD8783 <lelomlalol@gmail.com>
2020-06-11 04:40:41 -0500

    push ae

--- UI/Elements/UIHoverBigTextWithBorder.cs
+++ UI/Elements/UIHoverBigTextWithBorder.cs
@@ -1,20 +1,32 @@
 ﻿using AssortedModdingTools.DataStructures;
-using AssortedModdingTools.UI.Elements;
 using Microsoft.Xna.Framework;
 using Terraria.UI;
 
 namespace AssortedModdingTools.UI.Elements
 {
-	//todo hovering lol
-	public class UIHoverBigTextWithBorder : UIBigTextWithBorder
-	{
-		public UIHoverBigTextWithBorder(string text, TextBorderHoverColors? textBorderHoverColors = null, Vector2? origin = null, float scale = 1f) 
-			: base(text, textBorderHoverColors.Value.TextBorderColors, origin, scale) { }
+    //todo hovering lol
+    public class UIHoverBigTextWithBorder : UIBigTextWithBorder
+    {
+        public UIHoverBigTextWithBorder(string text, TextBorderHoverColors? textBorderHoverColors = null, Vector2? origin = null, float scale = 1f)
+            : base(text, textBorderHoverColors.Value.TextBorderColors, origin, scale) { }
 
-		public override void MouseOver(UIMouseEvent evt)
-		{
-			base.MouseOver(evt);
+        public override void MouseOver(UIMouseEvent evt)
+        {
+            base.MouseOver(evt);
 
-		}
-	}
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (IsMouseHovering)
+            {
+                if (scale < 1)
+                    scale += 0.2f;
+                if (scale > 1)
+                    scale = 1;
+            }
+            else if (scale > 0.8f)
+                scale -= 0.02f;
+        }
+    }
 }
---
+++ UI/States/Menu/UIInterfaceSettings.cs
@@ -0,0 +1,30 @@
+﻿using AssortedModdingTools.UI.Elements;
+using Terraria;
+using Terraria.Localization;
+using Terraria.UI;
+
+namespace AssortedModdingTools.UI.States.Menu
+{
+    public class UIInterfaceSettings : UIState
+    {
+        public override void OnInitialize()
+        {
+            base.OnInitialize();
+            UIBigTextWithBorder pickuptext = new UIBigTextWithBorder(Main.showItemText ? Language.GetTextValue("LegacyMenu.71") : Language.GetTextValue("LegacyMenu.72"));
+            UIBigTextWithBorder eventprogressbar = new UIBigTextWithBorder(Language.GetTextValue($"LegacyMenu.123") + Language.GetTextValue($"LegacyMenu.{124 + Main.invasionProgressMode}"));
+            UIBigTextWithBorder placementPreview = new UIBigTextWithBorder(Main.placementPreview ? Language.GetTextValue("LegacyMenu.128") : Language.GetTextValue("LegacyMenu.129"));
+            UIBigTextWithBorder highlightnewitems = new UIBigTextWithBorder(ItemSlot.Options.HighlightNewItems ? Language.GetTextValue("LegacyInterface.117") : Language.GetTextValue("LegacyInterface.116"));
+            UIBigTextWithBorder tilegrid = new UIBigTextWithBorder(Main.MouseShowBuildingGrid ? Language.GetTextValue("LegacyMenu.229") : Language.GetTextValue("LegacyMenu.230"));
+            UIBigTextWithBorder gamepadInstructions = new UIBigTextWithBorder(Main.GamepadDisableInstructionsDisplay ? Language.GetTextValue("LegacyMenu.241") : Language.GetTextValue("LegacyMenu.242"));
+            UIBigTextWithBorder back = new UIBigTextWithBorder(Language.GetTextValue("LegacyMenu.5"));
+
+            Append(pickuptext);
+            Append(eventprogressbar);
+            Append(placementPreview);
+            Append(highlightnewitems);
+            Append(tilegrid);
+            Append(gamepadInstructions);
+            Append(back);
+        }
+    }
+}
