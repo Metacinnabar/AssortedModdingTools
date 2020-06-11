63a09167d98efe7935fd4031e696264ae8d19f87
GoodPro712 <charliebrannan154@gmail.com>
2020-06-11 13:55:24 +1000

    eeeeeeeeeeee

--- Extensions/MathExtensions.cs
+++ Extensions/MathExtensions.cs
@@ -5,10 +5,6 @@ namespace AssortedModdingTools.Extensions
 {
 	public static partial class Extensions
 	{
-		public static float Angle(this Vector2 vector) => (float)Math.Atan2(vector.Y, vector.X);
-
 		public static Vector2 Perpendicular(this Vector2 vector) => new Vector2(0f - vector.Y, vector.X);
-
-		public static float Angle(this Vector2 from, Vector2 to) => (float)Math.Atan2(to.Y - from.Y, to.X - from.X);
 	}
 }
